5b8b440d06eb61cd9909b74e7f53e8b04d7cb80f
Paul Roode <44099555+pauliwag@users.noreply.github.com>
2021-02-12 03:17:05 -0500

    Delete src/main/java/com/lab4 directory

--- src/main/java/com/lab4/AddressBookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lab4;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface AddressBookRepository extends PagingAndSortingRepository<AddressBook, Long> {
-    AddressBook findById(long id);
-}
